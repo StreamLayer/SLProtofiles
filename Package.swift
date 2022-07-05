99798ade70e8775d706131fdabff1659122d5cfa
bimawa <bimawa@gmail.com>
2022-07-05 22:52:24 +0500

    feat(spm): init release SLProtofiles

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SLProtofiles",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SLProtofiles",
+            targets: ["SLProtofilesRelease"]),
+    ],
+    dependencies: [
+          .package(url: "git@github.com:grpc/grpc-swift.git", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SLProtofilesRelease",
+            dependencies: [
+              .product(name: "GRPC", package: "grpc-swift"),
+              .target(name: "SLProtofilesWrapper")
+            ]),
+        .target(
+          name: "SLProtofilesWrapper",
+          dependencies: [
+            .target(name: "SLProtofiles", condition: .when(platforms: [.iOS])),
+          ]
+        ),
+        .binaryTarget(
+            name: "SLProtofiles",
+            url: "https://github.com/StreamLayer/SLProtofiles/releases/download/v0.0.0/SLProtofiles.xcframework.zip",
+            checksum: "ea8ca09266ce4c0899d93b9eba4cd55112d9742d5aff22d02542346b29bc35d1"
+        ),
+    ]
+)
---
+++ Tests/SLProtofilesTests/SLProtofilesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SLProtofiles
+
+final class SLProtofilesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SLProtofiles().text, "Hello, World!")
+    }
+}
